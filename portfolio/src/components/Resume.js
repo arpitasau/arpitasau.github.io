42b9609997769719842cdd01e93785973af12d8f
Arpita K Sau <arpitakaran@gmail.com>
2020-09-28 17:41:12 -0500

    added-resume-component

---
+++ portfolio/src/components/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div>
+            hello from resume
+            
+        </div>
+    )
+}
+
+export default Resume
--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,19 +1,68 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Header from './Header'
-import Particles from "react-particles-js"
+import React from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Particles from "react-particles-js";
+import {makeStyles} from "@material-ui/styles";
+import { Opacity } from '@material-ui/icons';
+
+
+const useStyles = makeStyles ({
+    particlesCanva: {
+        position: "absolute",
+        opacity: "0.5"
+    }
+})
 
 const Home = () => {
+
+    const classes = useStyles()
     return (
         <>
             <Navbar/>
             <Header/>
             <Particles
+            canvasClassName = {classes.particlesCanva}
             params={{
                 particles: {
                     number: {
-                        value:45
+                        value:45,
+                        density: {
+                            enable: true,
+                            value_area: 900
+
+                       }
+                    },
+                    shape: {
+                        type: "circle",
+                        strole: {
+                            width: 1,
+                            color: "tomato"
+                        }
+
+                    },
+                    size: {
+                        value: 8,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 6,
+                            size_min: 0,
+                            sync:true,
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: false
+
+                        }
                     }
+
+
                 }
             }}
             />
