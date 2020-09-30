06f0e933ccae9adbf61699e401151e1367c9f0dc
Arpita K Sau <arpitakaran@gmail.com>
2020-09-30 10:33:29 -0500

    added-contact-component

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import {makeStyles, withStyles} from "@material-ui/core/styles";
+import {TextField, Typography, Button, Grid, Box} from "@material-ui/core"
 import Navbar from "./Navbar";
+import SendIcon from "@material-ui/icons/Send";
 
-function Contact() {
+const InputField = withStyles({
+    root: {
+        "& label.Mui-focused" : {
+            color: "tomato",
+        },
+        "& label": {
+            color: "tan",
+        },
+        "&.MuiOutlinedInput-root" : {
+            "& fieldset" : {
+                borderColor: "tan",
+            },
+            "&:hover fieldset" : {
+                borderColor: "tan",
+            }
+        }
+
+    }
+})
+
+const Contact = () => {
     return (
-        <>
-        <Navbar />
-            hello from contact
-        </>
+       <Box component="div">
+           <Grid container justify="center">
+               <Box component="form">
+                   <Typography variant="h5">
+                       Hire or contact me
+                   </Typography>
+                   {/* <InputField fullWidth={true} label="Name" variant="outlined" /> */}
+
+               </Box>
+
+           </Grid>
+       </Box>
     )
 }
 
-export default Contact
+export default Contact;
