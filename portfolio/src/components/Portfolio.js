7da623d62052a05be4d066097b033b5d5daedade
Arpita K Sau <arpitakaran@gmail.com>
2020-09-29 22:59:53 -0500

    added-project-portfolio

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";s
+import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "./Navbar";
+import project1 from "../images/codequiz.png";
+
 import {
     Box,
     Grid,
@@ -12,34 +14,271 @@ import {
     Button,
     Typography
 } from "@material-ui/core/";
+import { mergeClasses } from '@material-ui/styles';
 
-function Portfolio() {
+const useStyles = makeStyles({
+    mainContainer: {
+        height: "100%",
+    },
+    cardContainer: {
+        maxWidth: 345,
+        margin: "3rem",
+        margin: "5rem auto"
+    }
+
+})
+
+const Portfolio = () => {
+    const classes = useStyles();
     return (
-        <Box component="div">
+        <Box component="div" className={classes.mainContainer}>
             <Navbar />
-            <Grid container justify="center" alignItems="center">
+            <Grid container justify="center">
                 <Grid item xs={12} sm={8} md={6}>
-                <Card>
-                    <CardActionArea>
-                        <CardMedia
-                        component="img"
-                        alt="Project1"
-                        height="140"
-                        image={project1}
-                        >
-                        </CardMedia>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5">
-                                
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project1"
+                                height="140"
+                                image={project1}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 1
                             </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-              </Grid>
-            </Grid>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    share
+                            </Button>
+                                <Button size="small" color="primary">
+                                    live
+                            </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Project1"
+                                    height="140"
+                                    image={project1}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Project 1
+                            </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        share
+                            </Button>
+                                    <Button size="small" color="primary">
+                                        live
+                            </Button>
+                                </CardActions>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project1"
+                                height="140"
+                                image={project1}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 1
+                            </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    share
+                            </Button>
+                                <Button size="small" color="primary">
+                                    live
+                            </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Project1"
+                                    height="140"
+                                    image={project1}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Project 1
+                            </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        share
+                            </Button>
+                                    <Button size="small" color="primary">
+                                        live
+                            </Button>
+                                </CardActions>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project1"
+                                height="140"
+                                image={project1}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 1
+                            </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    share
+                            </Button>
+                                <Button size="small" color="primary">
+                                    live
+                            </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Project1"
+                                    height="140"
+                                    image={project1}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Project 1
+                            </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        share
+                            </Button>
+                                    <Button size="small" color="primary">
+                                        live
+                            </Button>
+                                </CardActions>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project1"
+                                height="140"
+                                image={project1}
+                            >
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 1
+                            </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    share
+                            </Button>
+                                <Button size="small" color="primary">
+                                    live
+                            </Button>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Project1"
+                                    height="140"
+                                    image={project1}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Project 1
+                            </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        agggjjkjkjjsjkjc jjcjjcj jkkkkjcjcj bvshjjhshchbhbas vhhcjhbbchbcb bhjbcbhbhcbb hbjcjbhbcjkbbjkc   c cbcbcbbcbjc.
+
+                            </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        share
+                            </Button>
+                                    <Button size="small" color="primary">
+                                        live
+                            </Button>
+                                </CardActions>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                </Grid>
             
-       </Box >
-        
+
+        </Box >
+
     )
 }
 
